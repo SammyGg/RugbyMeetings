816a4ade6aec55e5b019a48d11a29b67ae9b4b12
SammyGg <samuelgascoyne@outlook.com>
2024-02-19 19:02:56 +0000

    Admin Interface

--- meeting_planner/meetings/models.py
+++ meeting_planner/meetings/models.py
@@ -1,6 +1,22 @@
 from django.db import models
+from datetime import time
 
 # Create your models here.
+class Room(models.Model):
+    Name = models.CharField(max_length=200)
+    floor_number= models.IntegerField(default=0)
+    room_number= models.IntegerField(default=1)
+    
+    def __str__(self):
+        return f"{self.Name} at {self.room_number} on {self.floor_number}"
+    
 class Meeting(models.Model):
     title = models.CharField(max_length=200)
     date=models.DateField()
+    start_time = models.TimeField(default=time(9))
+    duration = models.IntegerField(default=1)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.title} at {self.start_time} on {self.date}"
+    
