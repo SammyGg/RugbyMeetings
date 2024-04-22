193978a6ebd5174e900a26021b7c5d159c2e9be6
SammyGg <samuelgascoyne@outlook.com>
2024-04-22 19:19:36 +0100

    templatefix

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 from datetime import datetime
 
 def welcome(request):
-    return render(request,"webiste/welcome.html")
+    return render(request,"website/welcome.html",
+                  {"message": "This data was sent from the view to the template."})
 def date(request):
     return HttpResponse("This page was served at "+ str(datetime.now()))
 def about(request):
